bde29cb642ae75a3a849b68966983a5dfc621307
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2019-06-14 12:34:14 -0400

    Add `shuffleArray` function
    Receives an array and returns a shuffled copy. Original array is not
    touched.

---
+++ shuffleArray.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// Randomly shuffle an array
+// https://stackoverflow.com/a/2450976/1293256
+
+const shuffleArray = (array) => {
+  const arrayCopy = [ ... array ];
+  let currentIndex = arrayCopy.length;
+  let temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = arrayCopy[currentIndex];
+    arrayCopy[currentIndex] = arrayCopy[randomIndex];
+    arrayCopy[randomIndex] = temporaryValue;
+  }
+
+  return arrayCopy;
+};
+
+
+module.exports = shuffleArray;
